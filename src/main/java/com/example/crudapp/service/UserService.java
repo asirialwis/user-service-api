429b2a21bccb62db4fc4a2ff135fa1945ce931c3
asirialwis <asirichathurika900@gmail.com>
2024-10-02 19:14:42 +0530

    add service logic

---
+++ src/main/java/com/example/crudapp/service/UserService.java
@@ -0,0 +1,36 @@
+package com.example.crudapp.service;
+
+import com.example.crudapp.model.User;
+import com.example.crudapp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User>getAllUsers(){
+        return userRepository.findAll();
+    }
+    public Optional<User>getUserById(String id){
+        return userRepository.findById(id);
+    }
+    public User createUser(User user){
+        return userRepository.save(user);
+    }
+    public User updateUser(String id, User user){
+        if(userRepository.existsById(id)){
+            user.setId(id);
+            return userRepository.save(user);
+        }
+        return null;
+    }
+    public void deleteUser(String id){
+        userRepository.deleteById(id);
+    }
+
+}
