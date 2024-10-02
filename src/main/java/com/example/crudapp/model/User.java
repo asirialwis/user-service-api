3b2fa809f7373f28ee5d464bf33132cbd449e3de
asirialwis <asirichathurika900@gmail.com>
2024-10-02 18:42:55 +0530

    project setup and entity class add

---
+++ src/main/java/com/example/crudapp/model/User.java
@@ -0,0 +1,16 @@
+package com.example.crudapp.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document
+public class User {
+    @Id
+    private String id;
+    private String name;
+    private String email;
+    private int age;
+
+}
