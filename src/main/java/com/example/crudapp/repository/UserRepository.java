e2705ec8039c560bac1bfaa2494912e2d3b72d26
asirialwis <asirichathurika900@gmail.com>
2024-10-02 18:58:05 +0530

    repository class add

---
+++ src/main/java/com/example/crudapp/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.crudapp.repository;
+
+import com.example.crudapp.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
