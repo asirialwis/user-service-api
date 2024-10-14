29df7bb507abf0e510d32515946d1925be9987ef
asirialwis <asirichathurika900@gmail.com>
2024-10-14 16:40:12 +0530

    add service class

--- src/main/java/com/example/crudapp/model/Appointment.java
+++ src/main/java/com/example/crudapp/model/Appointment.java
@@ -1,10 +1,20 @@
 package com.example.crudapp.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Document
 public class Appointment {
     private int id;
     private String doctorName;
---
+++ src/main/java/com/example/crudapp/service/AppointmentService.java
@@ -0,0 +1,26 @@
+package com.example.crudapp.service;
+
+import com.example.crudapp.dto.AppointmentRequest;
+import com.example.crudapp.model.Appointment;
+import com.example.crudapp.repository.AppointmentRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class AppointmentService {
+    private final AppointmentRepository appointmentRepository;
+
+    //Create Appointment
+    public void createAppointment(AppointmentRequest appointmentRequest) {
+        Appointment appointment = Appointment.builder()
+                .doctorName(appointmentRequest.getDoctorName())
+                .Location(appointmentRequest.getLocation())
+                .paymentStatus(appointmentRequest.getPaymentStatus())
+                .build();
+
+        appointmentRepository.save(appointment);
+    }
+}
