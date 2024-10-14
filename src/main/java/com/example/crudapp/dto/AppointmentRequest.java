c1ccfe3dbf2ccde56796e0e4b7b1b7d476b09b98
asirialwis <asirichathurika900@gmail.com>
2024-10-14 16:23:24 +0530

    add AppointmentDto

---
+++ src/main/java/com/example/crudapp/dto/AppointmentRequest.java
@@ -0,0 +1,19 @@
+package com.example.crudapp.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Document
+public class AppointmentRequest {
+    private String doctorName;
+    private String Location;
+    private String paymentStatus;
+}
