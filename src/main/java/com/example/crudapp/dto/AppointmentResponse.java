c7ef162a904c2c67de81f70fc4eecf18e326e699
asirialwis <asirichathurika900@gmail.com>
2024-10-15 14:17:16 +0530

    respoce dto add

--- src/main/java/com/example/crudapp/dto/AppointmentResponse.java
+++ src/main/java/com/example/crudapp/dto/AppointmentResponse.java
@@ -1,26 +1,21 @@
 package com.example.crudapp.dto;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.core.mapping.Document;
-
-
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Document
-public class AppointmentRequest {
+public class AppointmentResponse {
+    private String id;
     private String doctorName;
     private String location;
     private String paymentStatus;
     private String category;
     private String appointmentStatus;
     private ObjectId userid;
-
 }
